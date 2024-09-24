5fe6bd7c18c237c15c2da56400f5481cdf589b1f
minakhoshkhoo <movaghar7482mina@gmail.com>
2024-09-24 13:55:52 -0700

    #update

--- TaskManager/HomePage.xaml.cs
+++ TaskManager/HomePage.xaml.cs
@@ -4,28 +4,87 @@ namespace TaskManager;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
-    //private async void groupButton(object sender, EventArgs e)
-    //{
-    //    Navigation.PushAsync(new GroupPage());
-    //}
+    private bool _isFlyoutVisible = false;
 
-    private async void peopleButton(object sender, EventArgs e)
+    public HomePage()
     {
-        Navigation.PushAsync(new MemberPage());
+        InitializeComponent();
     }
 
-    private async void domainButton(object sender, EventArgs e)
+    private async void OnMenuClicked(object sender, EventArgs e)
     {
-        Navigation.PushAsync(new Domain());
+        if (_isFlyoutVisible)
+        {
+            await FlyoutMenu.TranslateTo(-250, 0, 250, Easing.SinIn);
+            FlyoutMenu.IsVisible = false;
+        }
+        else
+        {
+            FlyoutMenu.IsVisible = true;
+            await FlyoutMenu.TranslateTo(0, 0, 250, Easing.SinIn);
+        }
 
+        _isFlyoutVisible = !_isFlyoutVisible;
+    }
+    private void ProfileClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ProfilePage());
     }
 
-    private async void tasksButton(object sender, EventArgs e)
+    private void ProfileImageClicked(object sender, EventArgs e)
     {
+        Navigation.PushAsync(new ProfilePage());
+    }
+    private void TasksImageClicked(object sender, EventArgs e)
+    {
+
         Navigation.PushAsync(new Tasks());
     }
+
+    private void TasksClicked(object sender, EventArgs e)
+    {
+
+        Navigation.PushAsync(new Tasks());
+    }
+    private void GroupsImageClicked(object sender, EventArgs e)
+    {
+
+        Navigation.PushAsync(new GroupPage());
+    }
+
+    private void GroupsClicked(object sender, EventArgs e)
+    {
+
+        Navigation.PushAsync(new GroupPage());
+    }
+    private void peopleButton(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MemberPage()); ;
+
+    }
+
+    private void domainButton(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Domain()); ;
+    }
+
+    private void taskButton(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Tasks());
+
+    }
+    private async void OnMainContentTapped(object sender, EventArgs e)
+    {
+        // close a menu
+        await FlyoutMenu.TranslateTo(-250, 0, 250, Easing.SinIn);
+        FlyoutMenu.IsVisible = false;
+        _isFlyoutVisible = false;
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
+
+
---
+++ TaskManager/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TaskManager;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
